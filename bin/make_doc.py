d99f9de2a2f7c5824b337ae3771385b88bc3d656
Junya Hayashi <ledmonster@gmail.com>
2012-11-15 00:26:02 +0100

    Modified making rst logic

--- bin/make_doc.py
+++ bin/make_doc.py
@@ -3,8 +3,10 @@
 Script for creating rst files for each raw documents.
 """
 
+import re
 import glob
 import os.path
+import lxml.html
 
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 raw_dir = os.path.join(root_dir, 'raw')
@@ -12,18 +14,40 @@ doc_dir = os.path.join(root_dir, 'doc')
 
 def main():
     files = glob.glob("%s/*/*.html" % raw_dir)
-    for f in files:
-        dirs = f.split('/')
+    for fpath in files:
+        dirs = fpath.split('/')
 
         # files and dirs
         sub_dir = dirs[-2]
         raw_file = dirs[-1]
+        doc_id = raw_file.replace('.html', '')
         rst_file = raw_file.replace('html', 'rst')
         rst_dir = os.path.join(doc_dir, sub_dir)
 
-        # rst content
-        rst_content = ".. raw:: html\n    :file: ../../raw/%s/%s\n" % (sub_dir, raw_file)
+        # open raw file and get dom
+        fp = open(fpath)
+        html = fp.read().decode('utf-8')
+        fp.close()
+        doc = lxml.html.fromstring(html)
 
+        # get elements
+        title = doc.xpath('//title')[0].text_content()
+        body = lxml.html.tostring(doc.xpath('/html/body')[0], encoding='utf-8').decode('utf-8')
+        body = body.replace('<body>', '').replace('</body>', '')
+
+        # rst lines and content
+        rst_lines = list()
+        rst_lines.append('.. _%s:' % doc_id)
+        rst_lines.append('')
+        rst_lines.append("=" * (len(title) * 2))
+        rst_lines.append(title)
+        rst_lines.append("=" * (len(title) * 2))
+        rst_lines.append('')
+        rst_lines.append('.. raw:: html')
+        rst_lines.append(re.sub("^", "    ", body, 0, re.MULTILINE))
+        rst_content = "\n".join(rst_lines).encode('utf-8')
+
+        # write to rst file
         if not os.path.exists(rst_dir):
             os.makedirs(rst_dir)
 
